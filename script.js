536583e6dce5621dfbcd31a2c77cce0c54a4df9b
exoxaierist <exoxaierist@naver.com>
2022-12-15 14:06:31 +0900

    mobile

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let itemContainer = document.querySelector('#itemContainer');
 let items = document.querySelectorAll('#item');
 let contentContainer = document.querySelector('#contentContainer');
 let exitBtn = document.querySelector('#exitBtn');
+let arrowUp = document.querySelector('#arrowUp');
 let arrowDown = document.querySelector('#arrowDown');
 
 let names = document.querySelectorAll('#name');
@@ -12,15 +13,21 @@ let secondImgs = document.querySelectorAll('#secondImg');
 let counter = document.querySelector('#counter');
 let subCounter = document.querySelector('#subCounterContainer');
 
-let isMenu = false;
+let cursor = document.querySelector('#cursor');
+let cursorTexts = document.querySelectorAll('#cursorText');
+
+let isMenu = true;
 let index = 0;
 
 for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener('click',(e)=>ItemClick(Array.from(items).indexOf(e.currentTarget)))
+    items[i].addEventListener('click',(e)=>ItemClick(Array.from(items).indexOf(e.currentTarget)));
+    items[i].addEventListener('mouseover',(e)=>ItemHover(Array.from(items).indexOf(e.currentTarget)));
+    items[i].addEventListener('mouseout',ItemLeaveHover);
 }
 exitBtn.addEventListener('click',ToggleItemMode);
 document.addEventListener('scroll',()=>CalcNumber());
 addEventListener('resize',()=>CenterItemContainer());
+document.onmousemove = FollowMouse;
 
 CenterItemContainer();
 
@@ -32,17 +39,39 @@ function CenterItemContainer(){
     itemContainer.style.marginLeft = (extra+window.innerWidth*(5/100))+"px";
 }
 
+function FollowMouse(e){
+    cursor.style.top = e.clientY + "px";
+    cursor.style.left = e.clientX + "px"
+}
+
+function ItemHover(i){
+    if(!isMenu) return;
+    document.body.style.cursor = "none";
+    cursor.classList.add('cursorActive');
+    cursorTexts[i].classList.add('cursorTextActive');
+}
+
+function ItemLeaveHover(){
+    cursor.classList.remove('cursorActive');
+    for (let i = 0; i < cursorTexts.length; i++) {
+        cursorTexts[i].classList.remove('cursorTextActive');    
+    }
+}
+
 function ItemClick(i){
     ToggleItemMode();
     FocusOnItem(i);
 }
 
 function ToggleItemMode(){
+    isMenu = !isMenu
     contentContainer.classList.toggle('contentContainerActive');
     itemContainer.classList.toggle('itemContainerActive');
 }
 
 function FocusOnItem(i){
+    if(isMenu) return;
+    document.body.style.cursor = "crosshair";
     for (let i = 0; i < names.length; i++) names[i].classList.add('hidden');
     for (let i = 0; i < brands.length; i++) brands[i].classList.add('hidden');
     for (let i = 0; i < firstImgs.length; i++) firstImgs[i].classList.add('hidden');
@@ -52,7 +81,6 @@ function FocusOnItem(i){
     brands[i%brands.length].classList.remove('hidden');
     firstImgs[i%firstImgs.length].classList.remove('hidden');
     secondImgs[i%secondImgs.length].classList.remove('hidden');
-    arrowDown.classList.remove('arrowHidden');
     index = i;
     CalcNumber();
     window.scrollTo(0,0);
@@ -60,7 +88,10 @@ function FocusOnItem(i){
 
 function CalcNumber(){
     if(isMenu) return;
+    if(window.scrollY/window.innerHeight < 0.9) arrowUp.classList.add('arrowHidden');
+    else arrowUp.classList.remove('arrowHidden');
     if(window.scrollY/window.innerHeight > 0.1) arrowDown.classList.add('arrowHidden');
+    else arrowDown.classList.remove('arrowHidden');
     var t = 1-Math.pow(1-clamp((window.scrollY/window.innerHeight)*1.2 - 0.1,0,1),6);
     var n = lerp(firstImgs[index].getAttribute('price'),secondImgs[index].getAttribute('price'),t);
 
